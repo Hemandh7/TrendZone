9b75bbc695539d4dc6d5a68e668329b024cdc4b2
Hemandh <hemandh007bond@gmail.com>
2023-01-18 23:28:20 +0530

    day2 prograss

---
+++ trendzone/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../Components/navbar'
+import '../CSS/home.css'
+const Home = () => {
+    
+    
+  return (
+    <div>
+      <Navbar/>
+        <div className="banner">
+        </div><div className='sec1'></div>
+    </div>
+  )
+}
+
+export default Home
